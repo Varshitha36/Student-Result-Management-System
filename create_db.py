f27fd06a27007f970afa709d186fe0b5924db1d2
SaiVarshitha <153372585+Varshitha36@users.noreply.github.com>
2024-11-02 15:43:32 +0530

    Update create_db.py

--- create_db.py
+++ create_db.py
@@ -3,7 +3,7 @@ import mysql.connector
 def create_db():
     try:
         # Connect to MySQL
-        mydb = mysql.connector.connect(host='localhost', user='root', password='varsha', database='firstproject')
+        mydb = mysql.connector.connect(host='localhost', user='root', password='----', database='firstproject')
         cur = mydb.cursor()
 
         # Create 'course' table
